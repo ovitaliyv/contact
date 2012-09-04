41556842162d8dcb7ea3caf974ff17298b593a19
vetalreal <vetalreal@ukr.net>
2012-09-04 17:03:47 +0300

    Подключен и настроен Lunetics\LocaleBundle Настроен переключатель языков: - css - картинки флагов

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -28,6 +28,7 @@ class AppKernel extends Kernel {
         new Sonata\AdminBundle\SonataAdminBundle(),
         new Knp\Bundle\MenuBundle\KnpMenuBundle(),
         new Sonata\DoctrineORMAdminBundle\SonataDoctrineORMAdminBundle(),
+        new Lunetics\LocaleBundle\LuneticsLocaleBundle(),
     );
 
     if (in_array($this->getEnvironment(), array('dev', 'test'))) {
