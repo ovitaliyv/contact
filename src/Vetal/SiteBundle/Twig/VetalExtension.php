62b69048a46d9973f9b4412a4a6a0289109691bd
vetalreal <vetalreal@ukr.net>
2012-09-08 22:13:43 +0300

    Решена проблема с правильным формированием URL для переключения языка

--- src/Vetal/SiteBundle/Twig/VetalExtension.php
+++ src/Vetal/SiteBundle/Twig/VetalExtension.php
@@ -3,13 +3,16 @@
 namespace Vetal\SiteBundle\Twig;
 
 use Twig_Extension;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 use Doctrine\ORM\EntityManager;
 
 class VetalExtension extends Twig_Extension {
 
+  protected $container;
   protected $em;
 
-  public function __construct(EntityManager $em) {
+  public function __construct(EntityManager $em, ContainerInterface $container) {
+    $this->container = $container;
     $this->em = $em;
   }
 
@@ -24,6 +27,16 @@ class VetalExtension extends Twig_Extension {
     return array(
         'categories' => $categories,
     );
+  }
+
+  public function getFunctions() {
+    return array(
+        'param' => new \Twig_Function_Method($this,'getParam'),
+    );
+  }
+
+  public function getParam($paramName) {
+    return $this->container->getParameter($paramName);
   }
 
   public function getName() {
